e5a2210f3fbaf668b93066c52541955a6c0e6da4
Hoàng Tân <153097294+HoangTan2004@users.noreply.github.com>
2024-12-09 16:06:49 +0700

    debug

--- project/src/queue.c
+++ project/src/queue.c
@@ -20,14 +20,15 @@ struct pcb_t * dequeue(struct queue_t * q) {
          * in the queue [q] and remember to remove it from q
          * */
 	//return NULL;
-        if (q->size == 0) return NULL;
+    if (empty(q))
+        return NULL; // Return NULL if the queue is empty
 
-    struct pcb_t *proc = q->proc[0];
-    for (int i = 1; i < q->size; i++) {
-        q->proc[i - 1] = q->proc[i];
-    }
-    q->size--;
-    return proc;                   // Increase the size of the queue
-    
+    struct pcb_t *head = q->proc[0]; // Get the head of the queue
+
+    // Shift all the remained processes to the left
+    for (int i = 0; i < (q->size - 1); i++)
+        q->proc[i] = q->proc[i + 1];
+    q->proc[q->size - 1] = NULL; // Assign NULL value for the tail of the queue
+    q->size--;                   // Decrese the size of the queue
+    return head;                 // Return the head of the queue   
 }
-
